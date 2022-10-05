5a34ee83f5e3839ed516f0a9e41ab0c6897d0d56
jenniferboo <bugara.jennifer@gmail.com>
2022-10-05 13:52:37 +0200

    vari

---
+++ 02-comment.php
@@ -0,0 +1,15 @@
+<?php 
+// commentaire sur une ligne
+# une autre manière de faire des commentaire sur une ligne 
+/*commentaire sur pluiser ligne
+mais il doit etre fermer
+par */
+?>
+<!--
+    ceci est un commentaire html, il sera envoyaé au navigateur -->
+    <?php
+   // bonne pratique, ne pas fermer la balise php quand ce n'est pas utile 
+   // affichage avec echo, le ";" est la fin de l'instruction, on peut en mettre sans limite sur la meme ligne, ou separé les instructions par autant d'espace voulu php s'enfou
+   echo"<p> Temps Unix de la page:";
+   echo"</p>";
+   
---
+++ 03-variables-locales.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Variables locales</h1>
+
+<p>En PHP, les variables sont représentées par un signe dollar "$" suivi du nom de la variable. Le nom est sensible à la casse.<br>
+Les noms de variables suivent les mêmes règles de nommage que les autres entités PHP. Un nom de variable valide doit commencer par une lettre ou un souligné (_), suivi de lettres, chiffres ou soulignés. Exprimé sous la forme d'une expression régulière, cela donne : ^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*$</p>
+<p>on va éviter les caractère spéciaux, meme si ils sont valides. il faut absolument éviter les signes servant aux calcules</p>
+    <h2>Utilité des variables</h2>
+
+<p>Elles servent à stocker des information qui peuvent etre manipulées, affichées, sauvegardées, détruites etc..</p>
+</body>
+</html>
