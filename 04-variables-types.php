63ed320736ff7904f8f94a70863c2ac557dc8898
AnamariaTi <tirdeaana@outlook.fr>
2022-10-06 11:04:43 +0200

    de outile de debougage et type of var (int , double , string , long

--- 04-variables-types.php
+++ 04-variables-types.php
@@ -23,11 +23,55 @@ echo "le type e la variable est ".gettype($monTitre);  '!' ;
 
 
 ?>
+
 <p>Il existe des centaines de fonctions  pour gérer du texte , par exemple </p>
 <?php
 echo 'mettre tout en majuscule : strtoupper($monTitre '.strtoupper($monTitre); 
 ?>
 <p><a href="https://www.php.net/manual/fr/book.strings.php" target="_blank">string</a></p>
+
+
+
+<h2>Les numerique </h2>
+<p>Les numerique sont  les variable le plus utiliser pour le calculs , il existe plusieurs sous-types de ceux-ci  </p>
+<h3>Les entier (integer, int )</h3> 
+<p>Sont des entiers les nomber sans virglues ,positif comme negative </p>
+<?php
+        $myFirstInt = 22;     /** on pas besoin mettre le mot cle int  */
+        $mySecondInt = -7; 
+    
+        echo $myFirstInt; 
+        echo $mySecondInt; 
+        echo $monTitre;  
+        
+        //adition
+        $add=$myFirstInt + $mySecondInt ; 
+        echo $add ; 
+
+         //soustraction 
+         $sous =$myFirstInt -$mySecondInt ; 
+         echo $sous; 
+
+         // multiplication 
+         $multi =$myFirstInt * $mySecondInt;
+         echo $multi; 
+
+         //division 
+         $div =$myFirstInt / $mySecondInt ; 
+         echo $div; 
+         
+
+     // outile de débougage 
+     var_dump($myFirstInt;$mySecondInt,$monTitre, $add, $sous,$multi,$div ); 
+        
+?>
+<h3> 
+
+
+
+
+
+
 
 </body>
 </html>
