647b2ce3c0a8961b6ee3afdc6556e5b719a7ebd4
Magib <112185392+mageeeb@users.noreply.github.com>
2022-10-27 15:53:52 +0200

    20exo-magib-php

--- 20-magib-exe/pages/classcf.php
+++ 20-magib-exe/pages/classcf.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,16 +8,26 @@
     <title>Classes CF2m</title>
     <link rel="stylesheet" href="css/myCSS.css">
 </head>
+
 <body>
     <h1>Classes CF2m</h1>
-    <ul class="nav">
-        <li><a href="./">Accueil 20</a>
-        <li><a href="?u=formateurs">Formateurs WEB</a></li>
-        <li><a href="?u=stagiaires">Stagiaires WEB</a></li>
-        <li><a href="?u=classes">Classes CF2m</a></li>
-    </ul>
+    <?php
+    include "menu.php"
+    ?>
     <container>
         <h2>Liste des Classes du CF2m</h2>
+        <?php
+        foreach (CLASS_CF2M as $item) :
+        ?>
+            <ul>
+                <li><?= $item ?></li>
+
+
+            </ul>
+        <?php
+        endforeach;
+        ?>
     </container>
 </body>
+
 </html>
--- 20-magib-exe/pages/stagweb.php
+++ 20-magib-exe/pages/stagweb.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,16 +8,26 @@
     <title>Stagiaires WEB</title>
     <link rel="stylesheet" href="css/myCSS.css">
 </head>
+
 <body>
     <h1>Stagiaires WEB</h1>
-    <ul class="nav">
-        <li><a href="./">Accueil 20</a>
-        <li><a href="?u=formateurs">Formateurs WEB</a></li>
-        <li><a href="?u=stagiaires">Stagiaires WEB</a></li>
-        <li><a href="?u=classes">Classes CF2m</a></li>
-    </ul>
+    <?php
+    include "menu.php"
+    ?>
     <container>
         <h2>Liste Stagiaires WEB</h2>
+        <?php
+        foreach (STAGIAIRES_WEB as $item) :
+        ?>
+            <ul>
+                <li><?= $item[0] ?></li>
+                <li><?= $item[1] ?></li>
+
+            </ul>
+        <?php
+        endforeach;
+        ?>
     </container>
 </body>
+
 </html>
