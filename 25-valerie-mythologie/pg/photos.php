391de1c6816fb5d481e09058a60f29856b335bef
valerie2107 <kinkamyl@hotmail.com>
2022-11-16 12:29:12 +0100

    midi

--- 25-valerie-mythologie/pg/photos.php
+++ 25-valerie-mythologie/pg/photos.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Freebie: 4 Bootstrap Gallery Templates</title>
+    <title>Gallery</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
@@ -28,47 +28,47 @@
         <div class="row">
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/big_buddah.jpg">
-                    <img src="img/big_buddah.jpg" alt="big_buddah">
+                    <img src="img/big_buddah.jpg" alt="big_buddah" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/cambridge_church.jpg">
-                    <img src="img/cambridge_church.jpg" alt="cambridge_church">
+                    <img src="img/cambridge_church.jpg" alt="cambridge_church" width="350px">
                 </a>
             </div>
             <div class="col-sm-12 col-md-4">
-                <a class="lightbox" href="ims/celtic_cross.jpg">
-                    <img src="ims/celtic_cross.jpg" alt="celtic_cross">
+                <a class="lightbox" href="img/celtic_cross.jpg">
+                    <img src="img/celtic_cross.jpg" alt="celtic_cross" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/passion_fatima.jpg">
-                    <img src="img/passion_fatima.jpg" alt="passion_fatima">
+                    <img src="img/passion_fatima.jpg" alt="passion_fatima" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/temple_koh_samui.jpg">
-                    <img src="img/temple_koh_samui.jpg" alt="temple_koh_samui">
+                    <img src="img/temple_koh_samui.jpg" alt="temple_koh_samui" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="9_mondes.jpg">
-                    <img src="9_mondes.jpg" alt="9_mondes_asgard">
+                <a class="lightbox" href="img/9_mondes.jpg">
+                    <img src="img/9_mondes.jpg" alt="9 mondes asgard" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/Yggdrasil.webp">
-                    <img src="img/Yggdrasil.webp" alt="Yggdrasil">
+                    <img src="img/Yggdrasil.webp" alt="Yggdrasil" width="350px"> 
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/tarot.jpg">
-                    <img src="img/tarot" alt="cartes tarot">
+                    <img src="img/tarot" alt="cartes tarot" width="350px">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
                 <a class="lightbox" href="img/vienna_athena_landmark.jpg">
-                    <img src="img/vienna_athena_landmark.jpg" alt="vienna_athena_landmark">
+                    <img src="img/vienna_athena_landmark.jpg" alt="vienna_athena_landmark" width="350px">
                 </a>
             </div>
         </div>
